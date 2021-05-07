233c5e15a3ab0d2d785197c23371d5c44b1b0b0e
ynoza <46010696+ynoza@users.noreply.github.com>
2021-05-06 20:09:55 -0400

    Image deletion and loading complete

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 ﻿import { FormBuilder, FormGroup } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 import { Component } from '@angular/core';
 
@@ -13,8 +13,9 @@ import { OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
     user: User;
     isAdmin: Boolean;
-    SERVER_URL = "http://localhost:4000/upload";
+    SERVER_URL = "http://localhost:4000";
     uploadForm: FormGroup; 
+    variableName=['http://localhost:4000/public/uploads/cat.png', 'http://localhost:4000/public/uploads/cat_2.0.png']
 
     constructor(private accountService: AccountService, private formBuilder: FormBuilder, private http: HttpClient) {
         this.user = this.accountService.userValue;
@@ -31,25 +32,20 @@ export class HomeComponent implements OnInit {
       }
       
 
-    // onFileSelect(event) {
-    //     if (event.target.files.length > 0) {
-    //         const file = event.target.files[0];
-    //         this.uploadForm.get('profile').setValue(file);
-    //     }
-    // }
-
-    // onSubmit() {
-    //     const formData = new FormData();
-    //     formData.append('myImage', this.uploadForm.get('profile').value);
-    //     console.log(formData);
-    //     this.http.post<any>(this.SERVER_URL, formData).subscribe(
-    //       (res) => console.log(res),
-    //       (err) => console.log(err)
-    //     );
-    //   }
-
     onRemoved(event){
         console.log(event.file);
+
+        const file = event.file;
+        this.uploadForm.get('profile').setValue(file);
+        let body = new HttpParams();
+        body = body.set('fileName', file.name);
+        // const formData = new FormData();
+        // formData.append('myImage', this.uploadForm.get('profile').value);
+        console.log(file.name);
+        this.http.post<any>(this.SERVER_URL+'/delete', body).subscribe(
+          (res) => console.log(res),
+          (err) => console.log(err)
+        );
     }
 
     onUploadFinished(event) {
@@ -62,7 +58,7 @@ export class HomeComponent implements OnInit {
         const formData = new FormData();
         formData.append('myImage', this.uploadForm.get('profile').value);
         console.log(formData);
-        this.http.post<any>(this.SERVER_URL, formData).subscribe(
+        this.http.post<any>(this.SERVER_URL+'/upload', formData).subscribe(
           (res) => console.log(res),
           (err) => console.log(err)
         );
@@ -71,6 +67,13 @@ export class HomeComponent implements OnInit {
     onUploadStateChanged(event){
         console.log(event.file);
     }
+
+    // getImages(){
+    //     this.http.get<any>(this.SERVER_URL+'/getUploads').subscribe(
+    //         (res) => console.log(res),
+    //         (err) => console.log(err)
+    //       );
+    // }
 
 }
 
